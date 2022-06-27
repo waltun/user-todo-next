463a0655a02f687dda875fec592142965b70daef
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-06-27 13:25:32 +0430

    Create navbar component and create styles

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
+import Navbar from "../components/homePge/navbar";
+
 export default function Home() {
-  return (
-    <div className="flex justify-center">
-      <p className="text-2xl font-bold">Home page</p>
-    </div>
-  );
+  return <Navbar />;
 }
