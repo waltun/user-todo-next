281d285d265b4ed1355175900bddbbf2d9b2b515
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-06-30 18:24:57 +0430

    Create admin dropdown state and view

--- components/layout/admin/sidebar.js
+++ components/layout/admin/sidebar.js
@@ -3,10 +3,18 @@ import {
   HomeIcon,
   NewspaperIcon,
   ChevronDownIcon,
+  ChevronUpIcon,
 } from "@heroicons/react/solid";
 import Link from "next/link";
+import { useState } from "react";
 
 const Sidebar = () => {
+  const [dropDown, setDropdown] = useState(false);
+
+  const handleDropdown = () => {
+    setDropdown(!dropDown);
+  };
+
   return (
     <div className="fixed w-64 md:flex hidden h-full font-IRANSans">
       <aside className="bg-white overflow-y-auto flex flex-col flex-grow border-l border-gray-200">
@@ -34,23 +42,32 @@ const Sidebar = () => {
         </div>
 
         <div>
-          <div className="flex items-center text-gray-500 p-2 hover:bg-gray-100 rounded-md hover:text-black cursor-pointer">
+          <div
+            className="flex items-center text-gray-500 p-2 hover:bg-gray-100 rounded-md hover:text-black cursor-pointer"
+            onClick={handleDropdown}
+          >
             <div>
               <NewspaperIcon className="w-5 h-5 text-yellow-400" />
             </div>
             <div className="flex justify-between w-full items-center">
               <span className="text-sm px-2">بخش مقالات</span>
-              <ChevronDownIcon className="h-5 w-5" />
+              {dropDown ? (
+                <ChevronUpIcon className="h-5 w-5" />
+              ) : (
+                <ChevronDownIcon className="h-5 w-5" />
+              )}
             </div>
           </div>
-          <div className="mt-1 bg-gray-50 rounded-md p-2">
-            <a className="block text-sm text-gray-600 py-2 px-4 hover:bg-gray-100 hover:text-black rounded-md">
-              مدیریت مقالات
-            </a>
-            <a className="block text-sm text-gray-600 py-2 px-4 hover:bg-gray-100 hover:text-black rounded-md">
-              ایجاد مقاله جدید
-            </a>
-          </div>
+          {dropDown ? (
+            <div className="mt-1 bg-gray-50 rounded-md p-2">
+              <a className="block text-sm text-gray-600 py-2 px-4 hover:bg-gray-100 hover:text-black rounded-md">
+                مدیریت مقالات
+              </a>
+              <a className="block text-sm text-gray-600 py-2 px-4 hover:bg-gray-100 hover:text-black rounded-md">
+                ایجاد مقاله جدید
+              </a>
+            </div>
+          ) : null}
         </div>
       </aside>
     </div>
