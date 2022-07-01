c26c7d961a4a00970929afdf1dd0a260ec527217
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-07-01 19:57:25 +0430

    Add redux to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import "../styles/globals.css";
-import HomeLayout from "../components/layout/home";
+import { Provider } from "react-redux";
+import { store } from "../store";
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
