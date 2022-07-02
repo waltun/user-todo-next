c53c8eb558d825c21e7c930223e614c791e4c960
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-07-02 16:29:39 +0430

    Create users list view in admin panel

--- components/layout/admin/sidebar.js
+++ components/layout/admin/sidebar.js
@@ -4,6 +4,7 @@ import {
   NewspaperIcon,
   ChevronDownIcon,
   ChevronUpIcon,
+  UserIcon,
 } from "@heroicons/react/solid";
 import Link from "next/link";
 import { useState } from "react";
@@ -41,7 +42,7 @@ const Sidebar = () => {
           </Link>
         </div>
 
-        <div>
+        <div className="p-2 space-y-1">
           <div
             className="flex items-center text-gray-500 p-2 hover:bg-gray-100 rounded-md hover:text-black cursor-pointer"
             onClick={handleDropdown}
@@ -68,6 +69,39 @@ const Sidebar = () => {
               <Link href="/admin-panel/articles/create">
                 <a className="block text-sm text-gray-600 py-2 px-4 hover:bg-gray-100 hover:text-black rounded-md">
                   ایجاد مقاله جدید
+                </a>
+              </Link>
+            </div>
+          ) : null}
+        </div>
+
+        <div className="p-2 space-y-1">
+          <div
+            className="flex items-center text-gray-500 p-2 hover:bg-gray-100 rounded-md hover:text-black cursor-pointer"
+            onClick={handleDropdown}
+          >
+            <div>
+              <UserIcon className="w-5 h-5 text-yellow-400" />
+            </div>
+            <div className="flex justify-between w-full items-center">
+              <span className="text-sm px-2">بخش کاربران</span>
+              {dropDown ? (
+                <ChevronUpIcon className="h-5 w-5" />
+              ) : (
+                <ChevronDownIcon className="h-5 w-5" />
+              )}
+            </div>
+          </div>
+          {dropDown ? (
+            <div className="mt-1 bg-gray-50 rounded-md p-2">
+              <Link href="/admin-panel/users">
+                <a className="block text-sm text-gray-600 py-2 px-4 hover:bg-gray-100 hover:text-black rounded-md">
+                  مدیریت کاربران
+                </a>
+              </Link>
+              <Link href="/admin-panel/users/create">
+                <a className="block text-sm text-gray-600 py-2 px-4 hover:bg-gray-100 hover:text-black rounded-md">
+                  ایجاد کاربر جدید
                 </a>
               </Link>
             </div>
--- pages/admin-panel/users/index.js
+++ pages/admin-panel/users/index.js
@@ -1,8 +1,61 @@
-export default function Home() {
-    return (
-      <div className="flex justify-center">
-        <p className="text-2xl font-bold">Admin Panel - Users List</p>
-      </div>
-    );
-  }
-  
+import AdminLayout from "../../../components/layout/admin";
+
+const Users = () => {
+  return (
+    <div className="bg-white shadow overflow-x-auto rounded-lg hidden md:block">
+      <table className="min-w-full">
+        <thead>
+          <tr className="bg-sky-200">
+            <th className="px-4 py-3 text-sm font-bold text-gray-800 text-center rounded-r-md">
+              نام کاربر
+            </th>
+            <th className="px-4 py-3 text-sm font-bold text-gray-800 text-center">
+              شماره تماس کاربر
+            </th>
+            <th className="px-4 py-3 text-sm font-bold text-gray-800 text-center">
+              ایمیل کاربر
+            </th>
+            <th className="px-4 py-3 text-sm font-bold text-gray-800 text-center">
+              نوع کاربر
+            </th>
+            <th scope="col" className="relative px-4 py-3 rounded-l-md">
+              <span className="sr-only">اقدامات</span>
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td className="px-4 py-3 whitespace-nowrap">
+              <p className="text-sm text-black text-center">پوریا مستعان</p>
+            </td>
+            <td className="px-4 py-3 whitespace-nowrap">
+              <p className="text-sm text-black text-center">09022228553</p>
+            </td>
+            <td className="px-4 py-3 whitespace-nowrap">
+              <p className="text-sm text-indigo-black text-center block">
+                pooriyamostaan1@gmail.com
+              </p>
+            </td>
+            <td className="px-4 py-3 whitespace-nowrap">
+              <p className="text-sm text-black text-center">کاربر ادمین</p>
+            </td>
+            <td className="px-4 py-3 space-x-3 space-x-reverse whitespace-nowrap">
+              <button className="px-4 py-1.5 text-xs bg-indigo-500 hover:bg-indigo-600 rounded-md text-white">
+                ویرایش
+              </button>
+              <button className="px-4 py-1.5 text-xs bg-red-500 hover:bg-red-600 rounded-md text-white">
+                حذف
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+Users.getLayout = (page) => {
+  return <AdminLayout>{page}</AdminLayout>;
+};
+
+export default Users;
