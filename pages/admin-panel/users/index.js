647172cecb288d1ed894b10c0679fa8a90b2d5da
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-07-02 16:43:13 +0430

    Create new component for user items admin panel

--- pages/admin-panel/users/index.js
+++ pages/admin-panel/users/index.js
@@ -1,4 +1,5 @@
 import AdminLayout from "../../../components/layout/admin";
+import UserItems from "../../../components/admin/users/items";
 
 const Users = () => {
   return (
@@ -24,30 +25,7 @@ const Users = () => {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td className="px-4 py-3 whitespace-nowrap">
-              <p className="text-sm text-black text-center">پوریا مستعان</p>
-            </td>
-            <td className="px-4 py-3 whitespace-nowrap">
-              <p className="text-sm text-black text-center">09022228553</p>
-            </td>
-            <td className="px-4 py-3 whitespace-nowrap">
-              <p className="text-sm text-indigo-black text-center block">
-                pooriyamostaan1@gmail.com
-              </p>
-            </td>
-            <td className="px-4 py-3 whitespace-nowrap">
-              <p className="text-sm text-black text-center">کاربر ادمین</p>
-            </td>
-            <td className="px-4 py-3 space-x-3 space-x-reverse whitespace-nowrap">
-              <button className="px-4 py-1.5 text-xs bg-indigo-500 hover:bg-indigo-600 rounded-md text-white">
-                ویرایش
-              </button>
-              <button className="px-4 py-1.5 text-xs bg-red-500 hover:bg-red-600 rounded-md text-white">
-                حذف
-              </button>
-            </td>
-          </tr>
+          <UserItems />
         </tbody>
       </table>
     </div>
