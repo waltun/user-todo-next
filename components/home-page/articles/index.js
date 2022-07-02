cab601ffa2d3de91e0444f904de1bb562e1447f0
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-07-02 13:00:12 +0430

    Show articles in home page article list

--- components/home-page/articles/index.js
+++ components/home-page/articles/index.js
@@ -1,6 +1,28 @@
 import ArticleList from "./articleList";
 
+import { useDispatch, useSelector } from "react-redux";
+import { setArticle } from "../../../store/slices/articleSlice";
+
+import axios from "axios";
+
+import { useEffect } from "react";
+
 const Articles = () => {
+  const dispatch = useDispatch();
+  const articles = useSelector((state) => state.article.list);
+
+  useEffect(() => {
+    const getArticles = async () => {
+      let res = await axios.get(
+        "https://6283d9436b6c317d5ba74d17.endapi.io/articles"
+      );
+
+      dispatch(setArticle(res.data.data));
+    };
+
+    getArticles();
+  }, []);
+
   return (
     <>
       <div className="max-w-5xl mx-auto font-IRANSans">
@@ -8,7 +30,15 @@ const Articles = () => {
           <p className="text-lg font-bold text-gray-600">لیست مقالات</p>
         </div>
         <div className="md:grid grid-cols-3 gap-4 mt-4">
-          <ArticleList />
+          {articles ? (
+            articles.map((article) => (
+              <ArticleList key={article.id} article={article} />
+            ))
+          ) : (
+            <p className="text-center text-lg fnt-bold">
+              چیزی برای نمایش وجود ندارد
+            </p>
+          )}
         </div>
       </div>
     </>
