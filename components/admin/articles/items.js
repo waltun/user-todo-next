3440d4fb68026d3779e04a390429e3288b9fa7cf
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-07-03 13:43:02 +0430

    Handle delete article from state and api

--- components/admin/articles/items.js
+++ components/admin/articles/items.js
@@ -1,6 +1,24 @@
 import Link from "next/link";
 
+import { useDispatch } from "react-redux";
+
+import { deleteArticle } from "../../../store/slices/articleSlice";
+
+import axios from "axios";
+
 const ArticleItems = ({ article }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = async () => {
+    await axios
+      .delete(
+        "https://6283d9436b6c317d5ba74d17.endapi.io/articles/" + article.id
+      )
+      .then((response) => {
+        dispatch(deleteArticle(article.id));
+      });
+  };
+
   return (
     <tr>
       <td className="px-4 py-3 whitespace-nowrap">
@@ -23,7 +41,10 @@ const ArticleItems = ({ article }) => {
         <button className="px-4 py-1.5 text-xs bg-indigo-500 hover:bg-indigo-600 rounded-md text-white">
           ویرایش
         </button>
-        <button className="px-4 py-1.5 text-xs bg-red-500 hover:bg-red-600 rounded-md text-white">
+        <button
+          className="px-4 py-1.5 text-xs bg-red-500 hover:bg-red-600 rounded-md text-white"
+          onClick={handleDelete}
+        >
           حذف
         </button>
       </td>
