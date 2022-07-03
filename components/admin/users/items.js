7b81285fe498affb0b71070c539873b2fad31624
Pooriya Mostaan <pooriyamostaan1@gmail.com>
2022-07-03 13:55:57 +0430

    Handle delete user from state and api

--- components/admin/users/items.js
+++ components/admin/users/items.js
@@ -1,4 +1,20 @@
+import { useDispatch } from "react-redux";
+
+import { deleteUser } from "../../../store/slices/userSlice";
+
+import axios from "axios";
+
 const UserItems = ({ user }) => {
+  const dispatch = useDispatch();
+
+  const handleDelete = async () => {
+    await axios
+      .delete("https://6283d9436b6c317d5ba74d17.endapi.io/users/" + user.id)
+      .then((response) => {
+        dispatch(deleteUser(user.id));
+      });
+  };
+
   return (
     <tr>
       <td className="px-4 py-3 whitespace-nowrap">
@@ -27,7 +43,10 @@ const UserItems = ({ user }) => {
         <button className="px-4 py-1.5 text-xs bg-indigo-500 hover:bg-indigo-600 rounded-md text-white">
           ویرایش
         </button>
-        <button className="px-4 py-1.5 text-xs bg-red-500 hover:bg-red-600 rounded-md text-white">
+        <button
+          className="px-4 py-1.5 text-xs bg-red-500 hover:bg-red-600 rounded-md text-white"
+          onClick={handleDelete}
+        >
           حذف
         </button>
       </td>
